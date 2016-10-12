2880249644e0623ed7464258766689bf936786e3
Adam Thuvesen <a.thuvesen@gmail.com>
2016-10-12 22:08:37 +0200

    Links added, styling the different pages/components-links

--- public/bundle.js
+++ public/bundle.js
@@ -25459,37 +25459,44 @@
 	var _require = __webpack_require__(159);
 
 	var Link = _require.Link;
+	var IndexLink = _require.IndexLink;
 
 
 	var Nav = React.createClass({
-	  displayName: 'Nav',
+	    displayName: 'Nav',
 
-	  render: function render() {
-	    return React.createElement(
-	      'div',
-	      null,
-	      React.createElement(
-	        'h2',
-	        null,
-	        'Nav Component'
-	      ),
-	      React.createElement(
-	        Link,
-	        { to: '/' },
-	        'Get Weather'
-	      ),
-	      React.createElement(
-	        Link,
-	        { to: '/about' },
-	        'About'
-	      ),
-	      React.createElement(
-	        Link,
-	        { to: '/examples' },
-	        'Examples'
-	      )
-	    );
-	  }
+	    render: function render() {
+	        return React.createElement(
+	            'div',
+	            null,
+	            React.createElement(
+	                'h2',
+	                null,
+	                'Nav Component'
+	            ),
+	            React.createElement(
+	                IndexLink,
+	                { to: '/', activeClassName: 'active', activeStyle: {
+	                        fontWeight: 'bold'
+	                    } },
+	                'Get Weather'
+	            ),
+	            React.createElement(
+	                Link,
+	                { to: '/about', activeClassName: 'active', activeStyle: {
+	                        fontWeight: 'bold'
+	                    } },
+	                'About'
+	            ),
+	            React.createElement(
+	                Link,
+	                { to: '/examples', activeClassName: 'active', activeStyle: {
+	                        fontWeight: 'bold'
+	                    } },
+	                'Examples'
+	            )
+	        );
+	    }
 	});
 
 	module.exports = Nav;
