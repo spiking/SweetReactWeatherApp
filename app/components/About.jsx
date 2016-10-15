2b9434daa22692faea35f91821dd30ed7bfe4fe7
Adam Thuvesen <a.thuvesen@gmail.com>
2016-10-15 10:56:11 +0200

    Phrase update

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,11 +1,14 @@
 var React = require('react');
 
 var About = React.createClass({
-  render: function () {
-    return (
-      <h3>About Component</h3>
-    )
-  }
+    render: function() {
+        return (
+            <div>
+                <h3>About Component</h3>
+                <p>Welcome to the about page!</p>
+            </div>
+        )
+    }
 });
 
 module.exports = About;
