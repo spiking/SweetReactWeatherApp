af0298f1664733b5fa17a80f26aafbb32d351139
Adam Thuvesen <a.thuvesen@gmail.com>
2016-10-15 10:52:57 +0200

    Change

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,14 @@
 var React = require('react');
 
 var Examples = React.createClass({
-  render: function () {
-    return (
-      <h3>Examples Component</h3>
-    )
-  }
+    render: function() {
+        return (
+            <div>
+                <h3>Examples</h3>
+                <p>Welcome to the examples page!</p>
+            </div>
+        )
+    }
 });
 
 module.exports = Examples;
