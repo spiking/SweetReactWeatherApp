c43fc4e00e5446e82fd2d81c1a387eb5473e2c72
Adam Thuvesen <a.thuvesen@gmail.com>
2016-10-17 21:51:54 +0200

    Typo

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactDOMServer = require('react-dom/server');
 
 // Popup alert
 var ErrorModal = React.createClass({
@@ -10,16 +12,13 @@ var ErrorModal = React.createClass({
         message: React.PropTypes.string.isRequired
     },
     componentDidMount: function() {
-        var modal = new Foundation.Reveal($('#error-modal'));
-        modal.open();
-    },
-    render: function() {
         var {title, message} = this.props;
-
-        return (
+        var modalMarkup = (
             <div id="error-modal" className="reveal tiny text-center" data-reveal="">
-                <h4>{title}</h4>
-                <p>{message}</p>
+                {/* <h4>{title}</h4>
+              <p>{message}</p> */}
+              <h4>Error</h4>
+              <p>The city you entered does not exist!</p>
                 <p>
                     <button className="button hollow" data-close="">
                         Ok
@@ -27,6 +26,21 @@ var ErrorModal = React.createClass({
                 </p>
             </div>
         );
+        // Foundation changes the DOM, react dont like it
+        var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+        $(ReactDOM.findDOMNode(this)).html($modal);
+
+
+        var modal = new Foundation.Reveal($('#error-modal'));
+        modal.open();
+    },
+    render: function() {
+        var {title, message} = this.props;
+
+        return (
+            <div></div>
+        )
+
     }
 });
 
